8fdac9b2ed616e66baaf795022a9af1b976a55cc
lutca1320 <lutca1320@gmail.com>
2022-11-18 00:25:22 +0900

    Clear Font & Setup Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/*.{js,ts,jsx,tsx}"],
+  content: ["./app/*.{js,ts,jsx,tsx}", "./components/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        nexa: ["Nexa", "sans-serif"],
-      },
+    colors: {
+      background: "#1c2128",
+      secondary: "#22272e",
+      primary: "#2d333b",
+      highlight: "#42474f",
+      transparent: "#00000000",
     },
   },
   plugins: [],
