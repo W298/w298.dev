dfe047744befe80135436091bf1d89864da013ca
lutca1320 <lutca1320@gmail.com>
2022-11-20 17:07:34 +0900

    Modify ProjectCard Layout

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,6 +1,6 @@
 import { Unity, Blender, Notion } from "@icons-pack/react-simple-icons";
 import { RepoIcon, GitCommitIcon } from "@primer/octicons-react";
-import ProjectTag from "./ProjectTag";
+import Tag from "./ProjectTag";
 
 export default function ProjectCard({ title }) {
   return (
@@ -12,35 +12,33 @@ export default function ProjectCard({ title }) {
         src="https://w298.me/static/media/bgImg.57b3dfc9.png"
         className="object-cover h-28 rounded-t-md"
       ></img>
-      <div className="h-40 p-6 flex flex-row items-center justify-between gap-8">
-        <div className="h-full flex flex-col justify-between">
-          <div className="font-extrabold text-2xl">{title}</div>
-          <div>
-            <div className="font-light text-sm">Road Management Game</div>
-            <div className="font-light text-sm">Develop With Unity</div>
+      <div className="h-44 p-6 flex flex-col">
+        <div className="flex-1 flex flex-row justify-between">
+          <div className="flex flex-col gap-2">
+            <div className="font-extrabold text-2xl">{title}</div>
+            <div>
+              <div className="font-light text-sm">
+                Traffic Management Game with Roads
+              </div>
+              <div className="font-light text-sm">Develop With Unity</div>
+            </div>
           </div>
+          <div className="max-w-[40%] flex flex-row flex-wrap justify-end content-start mt-1 gap-2">
+            <Tag Icon={Unity} title="Unity" color="black" />
+            <Tag Icon={Blender} title="Blender" color="#d57132" />
+            <Tag Icon={Unity} title="Unity" color="black" />
+          </div>
+        </div>
+        <div className="flex flex-row justify-between items-center">
           <div className="flex flex-row gap-2 items-center">
             <GitCommitIcon size={16} />
             <div className="font-light text-xs text-text-secondary -mt-[3px]">
               Last Commit 22.11.19
             </div>
           </div>
-        </div>
-        <div className="flex flex-col max-w-[45%] h-full justify-between items-end">
-          <div className="flex flex-row flex-wrap-reverse justify-end gap-1">
-            <ProjectTag Icon={Unity} title="Unity" color="black" />
-            <ProjectTag Icon={Blender} title="Blender" color="#d57132" />
-            <ProjectTag Icon={Unity} title="Unity" color="black" />
-            <ProjectTag Icon={Unity} title="Unity" color="black" />
-            <ProjectTag Icon={Blender} title="Blender" color="#d57132" />
-            <ProjectTag Icon={Unity} title="Unity" color="black" />
-            <ProjectTag Icon={Blender} title="Blender" color="#d57132" />
-            <ProjectTag Icon={Unity} title="Unity" color="black" />
-          </div>
-          <div className="w-full h-[1px] bg-layer-200"></div>
-          <div className="flex flex-row flex-wrap-reverse justify-end gap-1">
-            <ProjectTag Icon={RepoIcon} title="Github" color="#393939" full />
-            <ProjectTag Icon={Notion} title="Notion" color="#393939" full />
+          <div className="flex flex-row flex-wrap-reverse justify-end gap-2">
+            <Tag Icon={RepoIcon} title="Github" color="#393939" />
+            <Tag Icon={Notion} title="Notion" color="#393939" />
           </div>
         </div>
       </div>
--- components/ProjectTag.js
+++ components/ProjectTag.js
@@ -1,11 +1,11 @@
-export default function ProjectTag({ Icon, title, color, full = false }) {
+export default function Tag({ Icon, title, color, thin = false }) {
   return (
     <div
-      className="flex flex-row items-center py-0.25 px-1.5 rounded-full select-none cursor-pointer"
+      className="flex flex-row h-min items-center py-0.25 px-1.5 rounded-full select-none cursor-pointer"
       style={{ backgroundColor: color }}
     >
       <Icon color="white" className="p-1 w-6 h-6" />
-      {full && <div className="font-bold text-xs p-1">{title}</div>}
+      {!thin && <div className="font-bold text-xs p-1">{title}</div>}
     </div>
   );
 }
