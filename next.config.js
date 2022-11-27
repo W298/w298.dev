a0355d05106b9df5d148980ec54f4465436eaba7
lutca1320 <lutca1320@gmail.com>
2022-11-27 23:53:19 +0900

    Redirect Root to Project Page

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental:{appDir: true}
-}
+  experimental: { appDir: true },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/projects",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
