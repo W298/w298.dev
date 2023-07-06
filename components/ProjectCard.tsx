06c382cf60900f80551829b7300cae90cacecfe9
W298 <lutca1320@gmail.com>
2023-07-06 22:28:06 +0900

    Test Image Preload

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -26,7 +26,7 @@ interface LinkTagsProp {
 function imagePreload(urls: string[]) {
   urls.forEach((url) => {
     const img = new Image();
-    img.src = `${process.env.NEXT_PUBLIC_CDN_URL}${url}`;
+    img.src = `${url}`;
   });
 }
 
