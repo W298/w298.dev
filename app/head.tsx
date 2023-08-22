48354310d836ad8e86bc7eff49c49dadab473663
W298 <lutca1320@gmail.com>
2023-08-22 17:32:22 +0900

    Add Favicon

--- app/head.tsx
+++ app/head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="theme-color" content="#000000" />
       <meta name="description" content="Game & Software Development Blog." />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/imgs/favicon.png" />
     </>
   );
 }
