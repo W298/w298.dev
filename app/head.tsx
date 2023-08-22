d7c7d8d8c2f6074fee946cca2649d3f8ad076593
W298 <lutca1320@gmail.com>
2023-08-22 17:02:13 +0900

    Add Meta Tags

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,8 @@ export default function Head() {
     <>
       <title>W298.me</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="theme-color" content="#000000" />
+      <meta name="description" content="Game & Software Development Blog." />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
