6d3354053eb84d413d3c522c6c6260043ba9d474
W298 <lutca1320@gmail.com>
2023-08-25 21:59:08 +0900

    Add Root Loading

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import "../styles/loading.css";
+
+export default function Loading() {
+  return (
+    <div className="w-full flex justify-center pt-10">
+      <div className="dot-bricks"></div>
+    </div>
+  );
+}
